ddf422141647739f23233001876cb79bce5e734f
Softtanck <cqm422013>
2020-06-09 22:09:41 +0800

    Init commit.

---
+++ app/src/main/java/com/softtanck/disklogger/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.softtanck.disklogger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        // Example of a call to a native method
+//        sample_text.text = stringFromJNI()
+    }
+
+
+}
